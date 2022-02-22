5010a37336f35cc948dda612ab765130b0cc7542
niiquash <qua16006@byui.edu>
2022-02-21 17:15:21 -0700

    First commit. Kotlin tip calculator.

---
+++ src/Main.kt
@@ -0,0 +1,69 @@
+fun calculateTip(bill: Float, tipPercent: Float): Float {
+    return bill * (tipPercent / 100)
+}
+
+fun billsAndTipsLogbook(vararg billsAndTips: MutableList<Any?>) {
+    if (billsAndTips.isEmpty()) {
+        println("Your logbook is empty")
+    } else {
+        for (i in 1..billsAndTips.size) {
+            println("Tips you have paid so far: ")
+            println(billsAndTips[i-1])
+        }
+    }
+}
+
+fun main() {
+//  Declare some variables that will be used
+    val billsAndTips = mutableListOf<Any?>()
+    var userInput: String? = null
+    var tip = 0f
+    var tipCount = 0
+//  Display greeting
+    println("**************************************************")
+    println("Welcome to the Tip Calculator Application!")
+    println("**************************************************")
+
+//  Display options
+    println()
+    println("This application will allow you to calculate a tip to add to your bills.")
+    println()
+    println("**************************************************")
+
+    while (userInput != "3") {
+
+        println("What do you want to do: ")
+        println("\t1. View previous bills and tips")
+        println("\t2. Calculate new tip")
+        println("\t3. Quit")
+
+    //  Get user options
+        print(">")
+        userInput = readLine()
+    //  Carry out task
+        when (userInput) {
+            "1" -> billsAndTipsLogbook(billsAndTips)
+            "2" -> {
+                tipCount += 1
+                print("Enter the bill amount: ")
+                var bill = readLine()!!
+                var floatBill = bill.toFloat()
+
+                print("Enter the tip percentage: ")
+                var tipPercent = readLine()!!
+                var floatTip = tipPercent.toFloat()
+
+                tip = calculateTip(floatBill, floatTip)
+
+                println("A $tipPercent% tip of of your bill is $$tip")
+
+                billsAndTips.add(tip)
+            }
+            "3" -> println("Goodbye")
+            else -> {
+                println("This option is not recognized!")
+            }
+    }
+}
+
+}
